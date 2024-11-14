88c91393f7a8f06b3f6406a5d241be7c298caf72
et4life22 <45518899+et4life22@users.noreply.github.com>
2024-11-14 11:09:37 -0800

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,6 +1,5 @@
-# If you want to use Bootstrap 5, you should include the correct gem in Gemfile (bootstrap)
-# require 'bootstrap'  # Uncomment this line if you are using bootstrap 5
-
+#Bootstrap is used to style bits of the demo. Remove it from the config, gemfile and stylesheets to stop using bootstrap
+require 'bootstrap-sass'
 require "uglifier"
 
 # Activate and configure extensions
@@ -18,7 +17,6 @@ activate :autoprefixer do |prefix|
 end
 
 activate :livereload
-
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
@@ -34,6 +32,9 @@ activate :blog do |blog|
   blog.sources = "posts/{title}.html"
   blog.layout = "news-detail"
 end
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
 
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
@@ -55,7 +56,7 @@ end
 activate :directory_indexes
 
 helpers do
-  # helper to set background images with asset hashes in a style attribute
+  #helper to set background images with asset hashes in a style attribute
   def background_image(image)
     "background-image: url('" << image_path(image) << "')"
   end
@@ -84,6 +85,7 @@ configure :build do
   # Use Gzip
   activate :gzip
 
-  # Use asset hashes for caching (uncomment if you want to use it)
-  # activate :asset_hash
+  #Use asset hashes to use for caching
+  #activate :asset_hash
+
 end
